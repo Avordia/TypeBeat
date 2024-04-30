7ef069a3ffc98ae6de3ee4b426d5dc2a69040a0a
Avordia <aesopianproductions@gmail.com>
2024-04-30 20:56:54 +0800

    commit

---
+++ core/src/Behavior/Score.java
@@ -0,0 +1,33 @@
+package Behavior;
+
+public class Score {
+    float score=0;
+    int combo=1;
+    int comboDisplay;
+
+
+    public int getCombo() {
+        return combo;
+    }
+
+    public String getScoreString(){
+        return String.format("%.0f", this.score);
+    }
+
+    public String getComboString(){
+        comboDisplay=combo-1;
+        return ""+comboDisplay;
+    }
+
+    public void resetCombo(){
+        combo=1;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    public void incrementCombo(){
+        combo++;
+    }
+}
